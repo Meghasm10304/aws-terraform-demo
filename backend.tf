210c55f3fb06c74589af6a219655ec6453c7789f
Meghana M <meghasm10304@gmail.com>
2026-05-06 22:16:58 +0530

    Fix Terraform formatting

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "meghana-terraform-state"
     key            = "infra/terraform.tfstate"
-    region         = "us-east-1"   # must match bucket region
+    region         = "us-east-1" # must match bucket region
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ module "vpc" {
 }
 
 module "ec2" {
-  source             = "./modules/ec2"
-  vpc_id             = module.vpc.vpc_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
+  source            = "./modules/ec2"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
 }
 
 module "alb" {
